070156d94bccb872804b04c381043bb5bc5fce1b
Seolhwa <devseolhwa@gmail.com>
2024-08-13 19:01:30 +0900

    메인작업

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,65 @@
 $(function(){
-    
+
+    // 팝업존
+	$(".popupzoneSlider .sliderInner").slick({
+        autoplay: true,
+        speed: 500,
+        slidesToShow: 1,
+        variableWidth: false,
+        arrows: true,
+        dots: true,
+        focusOnChange: true,
+        accessibility: false,
+        prevArrow: $(".btnPrev"),
+        nextArrow: $(".btnNext"),
+        //slidesToScroll: 1,
+        //infinite: false,
+        //fade: true,
+        //false: true,
+        customPaging: function (slick, index) {
+            $(".popupzoneSlider .controlWrap .totalWrap span").text(slick.slideCount);
+		}
+	}).on("afterChange", function (event, index, slick, currentSlide) {
+        $(".popupzoneSlider .controlWrap .totalWrap strong").text((slick + 1));
+	});
+
+    $(".controlWrap .btnWrap").append("<button type='button' class='btnPause'>stop</button>");
+
+    $(".controlWrap .btnWrap button").on("click", function () {
+		if ($(this).hasClass("btnPause")) {
+			$(".popupzoneSlider .sliderInner").slick("slickPause");
+			$(this).removeClass("btnPause").addClass("btnPlay");
+			$(this).text("play");
+		} else if ($(this).hasClass("btnPlay")) {
+			$(".popupzoneSlider .sliderInner").slick("slickPlay");
+			$(this).removeClass("btnPlay").addClass("btnPause");
+			$(this).text("stop");
+		} else if ($(this).hasClass("btnPrev")) {
+			$(".popupzoneSlider .sliderInner").slick("slickPrev");
+		} else {
+			$(".popupzoneSlider .sliderInner").slick("slickNext");
+		}
+	});
+
+    // 센터소식
+    $(".thumbBtnGroup li").on("click", function () {
+		const index = $(this).index();
+		if($(this).hasClass("on") == false){
+			$(this).addClass("on").siblings().removeClass("on");
+			$(".thumbListGroup > li").stop().hide();
+			$(".thumbListGroup > li").eq(index).stop().fadeIn();
+			$(".thumbListGroup > li .aos-init").removeClass("aos-animate");
+			$(`[data-aos][data-aos][data-aos-duration="1000"], body[data-aos-duration="1000"] [data-aos]`).css("transition-duration","0s");
+			setTimeout(() => {
+				$(`[data-aos][data-aos][data-aos-duration="1000"], body[data-aos-duration="1000"] [data-aos]`).css("transition-duration","1s");
+				$(".thumbListGroup > li .aos-init").addClass("aos-animate");
+			} , 100);
+		}		
+	});
+
+
     // 비주얼 스와이프
-    let visualArray = ["01", "02", "03"];
+   /* let visualArray = ["01", "02", "03"];
     let visualSwiper = new Swiper(".visualSwiper", {
         effect : "fade",
         centeredSlides: true,
@@ -48,7 +106,7 @@ $(function(){
 
     // 협회소개 스와이프
     new Swiper(".introductionSlide", {
-        effect: 'fade',
+        //effect: 'fade',
         autoplay: {
             delay: 5000,
             disableOnInteraction: false,
@@ -98,5 +156,5 @@ $(function(){
             prevEl: ".swiper-button-prev",
         }
     });
-    
+     */
 });
