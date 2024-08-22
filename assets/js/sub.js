25ef430829eb059a0e548a485a93ffd6936a50a3
Seolhwa <devseolhwa@gmail.com>
2024-08-22 18:38:32 +0900

    메인+인사말

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -1,10 +1,8 @@
 $(function(){
     
-    //lnb
-	$("#subVisual .lnb li button").on("click", function(){
-		$(this).parent("li").toggleClass("active").siblings("li").removeClass("active");
-		$(this).next(".depth02").stop().slideToggle();
-		$(this).parents().siblings("li").find(".depth02").slideUp();
+    // snb
+	$(document).off("click", ".snbList a").on("click", ".snbList a", function(e) {
+		$(this).addClass("on").siblings("a").removeClass("on");
 	});
 
 });
