7d1654735dfd42b9418a7021b1c4c1263763309f
Seolhwa <devseolhwa@gmail.com>
2024-09-18 18:33:07 +0900

    ㅊㅊ

--- assets/js/sub.js
+++ assets/js/sub.js
@@ -5,4 +5,19 @@ $(function(){
 		$(this).addClass("on").siblings("a").removeClass("on");
 	});
 
+	// tab
+	$(".tabNav a").on("click", function (e) {
+		e.preventDefault();
+
+		var $this = $(this);
+		var opt = $this.attr("href");
+
+		$this.parent("li").addClass("on").siblings("li").removeClass("on");
+
+		if (opt && opt.charAt(0) === "#") {
+			$(".tabDetails").hide();
+			$(opt).show();
+		}
+	});
+
 });
